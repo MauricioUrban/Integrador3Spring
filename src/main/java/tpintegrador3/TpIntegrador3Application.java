6f9c1bc4ca8a454bf9bc4cd11814bd1201efd232
claudiocarmusciano <claudiocarmusciano@gmail.com>
2023-11-05 19:24:25 -0300

    main

--- src/main/java/tpintegrador3/TpIntegrador3Application.java
+++ src/main/java/tpintegrador3/TpIntegrador3Application.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+/**
+ * The type Tp integrador 3 application.
+ */
 @SpringBootApplication
 @EntityScan("tpintegrador3")
 public class TpIntegrador3Application {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
           SpringApplication.run(TpIntegrador3Application.class, args);
     }
